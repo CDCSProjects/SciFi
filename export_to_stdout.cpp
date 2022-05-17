84861680bb28090165ebda1dcb144d1016165722
Martin Poppinga <martin.poppinga@uni-hamburg.de>
2022-05-17 19:38:10 +0200

    bugfixes import export

--- export_to_stdout.cpp
+++ export_to_stdout.cpp
@@ -13,9 +13,18 @@ int main(int argc, char *argv[]){
   string dbname = argv[1];
   SciStore::Storage<> * st = new SciStore::Storage<>(dbname + "_asset",dbname + "_meta");
 
-  std::vector<std::string> ids = st->get_all_assets();
-  std::vector<string> res = st->get(ids);
-  std::cout << res[0];
+  std::vector<std::string> res;
+  if (argv[2] == "all") {
+    res = st->get_all_assets(0, "", 2);
+  } else {
+    res = st->get_by_constraint(argv[2], 0, "", 2);
+  }
+
+  for (int i = 0; i < res.size(); i++) {
+    std::cout << res[i];
+  }
+  //std::cerr << res.size() << std::endl;
+  //std::cout << res[0];
 
   return 0;
 }
--- import_path.cpp
+++ import_path.cpp
@@ -3,17 +3,20 @@
 
 int main(int argc, char *argv[]){
 
-  if (argc != 3) {
-    printf("Usage: %s <dbname> <importpath>\n", argv[0]);
+  if (!(argc == 3 || argc == 4)){
+    printf("Usage: %s <dbname> <importpath> [metadatafile]\n", argv[0]);
     return 1;
   }
 
   string dbname = argv[1];
   string importpath = argv[2];
+  string metadatapath = argv[3];
 
 
   SciStore::Storage<> * st = new SciStore::Storage<>(dbname + "_asset",dbname + "_meta");
   st->load_assets_from_directory(importpath, 0, 0, 0, 0, 0);
+  //st->load_metadata_from_file(metadatapath, "key");  // load metadata from file
+  st->load_metadata_advanced(metadatapath, 1, "key varchar, video varchar");  // load metadata from file
 
   return 0;
 }
