f80e03a44d2617702f7d12aed96a475555b77520
Annett <annett.ungethuem@uni-hamburg.de>
2022-07-25 17:07:39 +0200

    changed setup.py to match new folder structure

--- pythonwrapper/setup.py
+++ pythonwrapper/setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+#cython: language_level=3
 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-setup(ext_modules=[Extension("assetstore", 
-                             ["assetstorecython.pyx", "duckstore.cpp", "rocksstore.cpp"], language="c++", include_dirs = ['./../duckdb-0.3.1/duckdb-0.3.1/src/include', './rocksdb/include/'],
+setup(ext_modules=[Extension("scifi", 
+                             ["scificython.pyx", "./../backend/src/duckstore.cpp", "./../backend/src/rocksstore.cpp", "./../framework/src/metadata.cpp", "./../framework/src/assetstore.cpp"], language="c++", include_dirs = ['./../../duckdb-0.3.1/duckdb-0.3.1/src/include', './../rocksdb/include/', './..'],
                     libraries = ['rocksdb', 'duckdb_static', 'duckdb_re2', 'fmt', 'hyperloglog' ,'imdb', 'miniz','pg_query', 'sqlite3', 'sqlite3_api_wrapper_static', 'test_helpers' ,'utf8proc', 'pthread' ,'dl', 'z','curl','curlpp'],
-                    library_dirs = ['./../duckdb-0.3.1/duckdb-0.3.1/src','/mnt/c/Users/ann__/Documents/rocksdb/rocksdb/', '.'],
+                    library_dirs = ['./../../duckdb-0.3.1/duckdb-0.3.1/src','/mnt/c/Users/ann__/Documents/rocksdb/rocksdb/', '.'],
                     extra_compile_args = ['-std=c++17'])],
       cmdclass = {'build_ext': build_ext})
       
+      #, "./../framework/src/storage.cpp", "./../framework/src/metadata.cpp"
+      #, './..framework/include', './../backend/include'
