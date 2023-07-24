f8b43e7709e34a92c26766a30df783c45d767536
Annett <annett.ungethuem@uni-hamburg.de>
2023-07-24 16:58:06 +0200

    added libraries required by newer version of duckdb

--- pythonwrapper/setup.py
+++ pythonwrapper/setup.py
@@ -7,7 +7,7 @@ from Cython.Distutils import build_ext
 
 setup(ext_modules=[Extension("scifi", 
                              ["scificython.pyx", "./../backend/src/duckstore.cpp", "./../backend/src/rocksstore.cpp", "./../framework/src/metadata.cpp", "./../framework/src/assetstore.cpp"], language="c++", include_dirs = ['./../duckdb/src/include', './../rocksdb/include/', './..'],
-                    libraries = ['rocksdb', 'duckdb_static', 'duckdb_re2', 'duckdb_fmt', 'duckdb_hyperloglog' ,'imdb', 'duckdb_miniz','duckdb_pg_query', 'duckdb_sqlite3', 'sqlite3_api_wrapper_static', 'test_helpers' ,'duckdb_utf8proc', 'parquet_extension', 'duckdb_fastpforlib', 'duckdb_mbedtls', 'pthread' ,'dl', 'z','curl','curlpp', 'snappy'],
+                    libraries = ['rocksdb', 'duckdb_static', 'duckdb_re2', 'duckdb_fmt', 'duckdb_hyperloglog' ,'imdb', 'duckdb_miniz','duckdb_pg_query', 'duckdb_sqlite3', 'sqlite3_api_wrapper_static', 'test_helpers' ,'duckdb_utf8proc', 'parquet_extension', 'duckdb_fastpforlib', 'duckdb_mbedtls', 'duckdb_fsst', 'jemalloc_extension', 'pthread' ,'dl', 'z','curl','curlpp', 'snappy'],
                     library_dirs = ['./../duckdb/staticlibs','./../rocksdb', '.'],
                     extra_compile_args = ['-std=c++17'])],
       cmdclass = {'build_ext': build_ext})
